9940c47b70aeb09ed87d96737bd97c64503b5754
YunYeoJin <duwls0349@naver.com>
2022-08-23 00:38:25 +0900

    Feat: TextField return 버튼 클릭시 내용 입력 구현

--- Shopping/ShoppingTableViewController.swift
+++ Shopping/ShoppingTableViewController.swift
@@ -16,10 +16,12 @@ class ShoppingTableViewController: UITableViewController {
     }
     
     var tasks: Results<ShoppingList>!
-  
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        SearchTextField.delegate = self
         
         tasks = localRealm.objects(ShoppingList.self).sorted(byKeyPath: "shoppingContents")
         
@@ -35,18 +37,18 @@ class ShoppingTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingTableViewCell", for: indexPath) as! ShoppingTableViewCell
         
-       
+        
         
         if tasks?[indexPath.row].checkBox == false {
-            cell.checkBoxButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
-        } else {
             cell.checkBoxButton.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
-        }
-
-        if tasks?[indexPath.row].favorite == false {
-            cell.favoriteButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
         } else {
+            cell.checkBoxButton.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
+        }
+        
+        if tasks?[indexPath.row].favorite == false {
             cell.favoriteButton.setImage(UIImage(systemName: "star"), for: .normal)
+        } else {
+            cell.favoriteButton.setImage(UIImage(systemName: "star.fill"), for: .normal)
         }
         
         cell.contentsLabel.text = tasks[indexPath.row].shoppingContents
@@ -57,22 +59,18 @@ class ShoppingTableViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 30
-    }
-    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-
+        
         if editingStyle == .delete {
             
-        try! localRealm.write {
-            localRealm.delete(tasks[indexPath.row])
-            tableView.reloadData()
-        }
+            try! localRealm.write {
+                localRealm.delete(tasks[indexPath.row])
+                tableView.reloadData()
+            }
         }
         
     }
@@ -86,22 +84,22 @@ class ShoppingTableViewController: UITableViewController {
     @IBAction func AddButtonClicked(_ sender: UIButton) {
         
         let task = ShoppingList(shoppingContents: "\(SearchTextField.text ?? "")") // => Record를 추가하는 과정
-            
+        
         try! localRealm.write {
             localRealm.add(task) // => Create 하는 과정
             tasks = localRealm.objects(ShoppingList.self).sorted(byKeyPath: "shoppingContents")
             tableView.reloadData()
         }
- 
+        
     }
     
     @IBAction func checkBoxButtonClicked(_ sender: UIButton) {
         
         let task = tasks?[sender.tag]
+        
         if task?.checkBox == true {
             try! localRealm.write {
                 task?.checkBox = false
-                
             }
         } else {
             try! localRealm.write {
@@ -115,10 +113,10 @@ class ShoppingTableViewController: UITableViewController {
     @IBAction func favoriteButtonClicked(_ sender: UIButton) {
         
         let task = tasks?[sender.tag]
+        
         if task?.favorite == true {
             try! localRealm.write {
                 task?.favorite = false
-                
             }
         } else {
             try! localRealm.write {
@@ -133,3 +131,19 @@ class ShoppingTableViewController: UITableViewController {
     
 }
 
+extension ShoppingTableViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let task = ShoppingList(shoppingContents: SearchTextField.text ?? "")
+        
+        try! localRealm.write{
+            localRealm.add(task)
+            tasks = localRealm.objects(ShoppingList.self).sorted(byKeyPath: "shoppingContents")
+            tableView.reloadData()
+        }
+        
+        SearchTextField.text = ""
+        return true
+    }
+}
